411ff5fca5fa97d1be223b7ddbbe3a0a367d5810
Josue Loya <jloya077@gmail.com>
2018-11-14 22:56:55 -0800

    Added another query, shows how to do prepared statements in java

--- Dino.java
+++ Dino.java
@@ -44,6 +44,12 @@ public class Dino
             System.out.println("Displaying Search Options");
             userMenu(connection, input);
         }
+        else if(in == 10)
+        { 
+          System.out.println("Disconnecting From Database...");
+          disconnectDatabase(connection, input);
+          
+        }
         else if(in > 10)
         {
           System.out.println("Invalid value entered, please try again");
@@ -82,6 +88,23 @@ public class Dino
     conn = DriverManager.getConnection("jdbc:sqlite:Dino.db");
     return conn;
   }
+  public static void disconnectDatabase(Connection conn, Scanner input) throws SQLException
+  {
+    if(conn != null)
+    {
+      conn.close();
+    }
+  }
+  public static String formatString(String in) //formats String so that the input is always right no matter how the user types it
+  {
+    if(in != null && in.length() > 0)
+    {
+      in = in.toLowerCase();
+      in = in.substring(0,1).toUpperCase() + in.substring(1);
+    }
+    return in;
+
+  }
   public static void userMenu(Connection conn, Scanner input) throws SQLException, InterruptedException
   {
     int in = 0;
@@ -91,6 +114,7 @@ public class Dino
         System.out.println("-------User Menu--------");
         System.out.println("0: Exit User Menu");
         System.out.println("1: Display Complete Dinosaur Data");
+        System.out.println("2: Search by Specific Dinosaur");
         in = input.nextInt();
         if(in == 0)
         {
@@ -99,6 +123,10 @@ public class Dino
         else if(in == 1)
         {
             userQuery1(conn);
+        }
+        else if(in == 2)
+        {
+          userQuery2(conn, input);
         }
 
 
@@ -126,4 +154,76 @@ public class Dino
     stmt.close();
     rs.close();
   }
+  public static void userQuery2(Connection conn, Scanner input) throws SQLException
+  {
+
+    ResultSet result = null; //initialize ResultSet
+    PreparedStatement pre = null; //initialize prepared statement
+    Statement stmt = conn.createStatement(); //initialize statement
+    int dinoKey = 0; //initialize variables
+    String dinoName = "";
+    String res = "";
+
+    String quickFix = input.nextLine(); //for some reason input skips over, doing this fixes it.
+
+    String preStmt = 
+    "select d_name, l_nation, h_name, tp_yearsAgo from Dinosaur, location, habitat, timeperiod where l_dinokey like ? and d_dinokey = ? and h_key = d_habkey and d_timeperiod = tp_name and d_name = ?";
+
+    pre = conn.prepareStatement(preStmt);
+
+    System.out.println("Please Enter Dinosaur Name:");
+    dinoName = input.nextLine();
+    dinoName = formatString(dinoName); //format into correct form
+
+    res = "select d_name, d_dinokey from dinosaur where d_name = \'"+dinoName+"\'";
+    result = stmt.executeQuery(res);
+
+    if(result.next())
+    {
+      dinoName = result.getString("d_name"); //set variables
+      dinoKey = result.getInt("d_dinokey");
+    }
+    else
+    {
+      System.out.println("Dinosaur not in database."); //check in case dinosaur not in database or something else happens
+      System.out.println(dinoName);
+      return;
+    }
+    
+    pre.setString(1, "%" + dinoKey + "%"); //do this to fill in LIKE keyword
+    pre.setInt(2, dinoKey);
+    pre.setString(3, dinoName);
+
+    result = pre.executeQuery(); //execute prepared statement
+    //print out results
+    if(result.next())
+    {
+      System.out.println("----------------------------------------------------------------------");
+      System.out.println("Name: " + result.getString(1));
+      System.out.println("Habitat: " + result.getString(3));
+      System.out.println("Years Ago: " + result.getInt(4));
+    }
+    else
+    {
+      System.out.println("Error, Prepared Statement didn't execute"); //failsafe in case prepared statement failed
+      return;
+    }
+    result = pre.executeQuery();
+    System.out.print("Country(ies): ");
+    //do while loop for countries since there's multiple countries
+    while(result.next())
+    {
+      System.out.print(result.getString(2) + ", ");
+    }
+    System.out.println();
+    System.out.println("----------------------------------------------------------------------");
+
+    pre.close(); //close statements and resultset
+    stmt.close();
+    result.close();
+
+
+
+  }
+
 }
